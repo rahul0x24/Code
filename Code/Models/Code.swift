bf963f5a3ecbf82c17b49fa4136aa4b2d625d353
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-11-28 07:10:45 -0500

    Added methods to Code class to encode and decode a Code object as JSON.

--- Code/Models/Code.swift
+++ Code/Models/Code.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 class Code: Codable {
-    var content = ""
+    var contentString = ""
     
+    /// Encodes and instance of Code as a JSON object
+    /// - Returns: Encoded data
+    func getCodeAsJSON() -> Data? {
+        let encoder = JSONEncoder()
+        return try? encoder.encode(self)
+    }
+    
+    /// Decodes JSON object
+    /// - Parameter data: data
+    /// - Returns: boolean value
+    func use(data: Data) -> Bool {
+        let decoder = JSONDecoder()
+        guard let decoded = try? decoder.decode(Code.self, from: data) else { return false }
+        contentString = decoded.contentString
+        return true
+    }
 }
