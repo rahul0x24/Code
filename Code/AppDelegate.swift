af8217225ce0d9519f5b27a24a985047a9b42385
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-11-27 21:51:53 -0500

    Initial Commit

---
+++ Code/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Code
+//
+//  Created by Joshua Rutkowski on 11/27/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
