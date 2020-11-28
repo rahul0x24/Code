56905fbb30d5191f13a964f0f6d09f07bd6d00b8
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-11-28 08:35:56 -0500

    Conformed View Controller to SyntaxTextViewDelegate. • Updated editorTextView theme for highlighting

--- Code/View Controllers/ViewController.swift
+++ Code/View Controllers/ViewController.swift
@@ -4,13 +4,15 @@
 //
 //  Created by Joshua Rutkowski on 11/27/20.
 //
-
+import Sourceful
 import Cocoa
 
-class ViewController: NSViewController {
-    
-    @IBOutlet weak var editorTextView: NSTextView!
+class ViewController: NSViewController, SyntaxTextViewDelegate {
+
+    @IBOutlet weak var editorTextView: SyntaxTextView!
     @IBOutlet weak var outputTextView: NSTextView!
+    
+    var lexer = SwiftLexer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +23,7 @@ class ViewController: NSViewController {
         handleOpenDocumentOperation()
         
         editorTextView.delegate = self
-        editorTextView.font = NSFont.monospacedSystemFont(ofSize: 14.0, weight: .regular)
-        outputTextView.font = NSFont.monospacedSystemFont(ofSize: 14.0, weight: .regular)
+        editorTextView.theme = DefaultSourceCodeTheme()
     }
 
     func handleOpenDocumentOperation() {
@@ -36,14 +37,18 @@ class ViewController: NSViewController {
 
     func populateDocumentContent() {
         guard let content = representedObject as? Code else { return }
-        editorTextView.string = content.contentString
+        editorTextView.text = content.contentString
+    }
+    
+    func lexerForSource(_ source: String) -> Lexer {
+        return lexer
     }
 }
 
 extension ViewController: NSTextViewDelegate {
     func textViewDidChangeSelection(_ notification: Notification) {
         // Updates the contentString property of the Code instance with the text view's text
-        (representedObject as? Code)?.contentString = editorTextView.string
+        (representedObject as? Code)?.contentString = editorTextView.text
 
     }
 }
