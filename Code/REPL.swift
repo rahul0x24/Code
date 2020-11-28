6947d83af1c4d19ea73b6340b49e1f671537c5d5
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-11-28 13:28:54 -0500

    Updated REPL to observe reading and writing and not block the main thread

--- Code/REPL.swift
+++ Code/REPL.swift
@@ -10,11 +10,35 @@ import Foundation
 final class REPL {
     private let process = Process()
     private let stdInput = Pipe()
+    private let stdError = Pipe()
+    private let stdOutput = Pipe()
+    
+    private var stdOutputtoken: Any?
+    private var stdErrorToken: Any?
     
     init() {
         process.launchPath = "/usr/bin/swift"
         process.standardInput = stdInput.fileHandleForReading
+        process.standardOutput = stdOutput.fileHandleForWriting
+        process.standardError = stdError.fileHandleForWriting
+
+        stdOutputtoken = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: stdOutput.fileHandleForReading, queue: nil) { [unowned self](code) in
+            let data = self.stdOutput.fileHandleForReading.availableData
+            let string = String(data: data, encoding: .utf8)!
+            print(string)
+            self.stdOutput.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        }
+        
+        stdErrorToken = NotificationCenter.default.addObserver(forName: .NSFileHandleDataAvailable, object: stdError.fileHandleForReading, queue: nil) { [unowned self](code) in
+            let data = self.stdError.fileHandleForReading.availableData
+            let string = String(data: data, encoding: .utf8)!
+            print(string)
+            self.stdError.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        }
+        
         process.launch()
+        stdOutput.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        stdError.fileHandleForReading.waitForDataInBackgroundAndNotify()
     }
     
     func execute(_ code: String) {
