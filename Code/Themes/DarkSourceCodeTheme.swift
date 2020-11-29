239d3eb6f51faea929ee7964872a3ff45042e9db
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-11-29 15:49:44 -0500

    Minor formatting fixes

--- Code/Themes/DarkSourceCodeTheme.swift
+++ Code/Themes/DarkSourceCodeTheme.swift
@@ -14,18 +14,26 @@ public struct DarkSourceCodeTheme: SourceCodeTheme {
         
     }
     
+    /// Color for numbers in left-side gutter
     private static var lineNumbersColor: Color {
         return Color(red: 100/255, green: 100/255, blue: 100/255, alpha: 1.0)
     }
     
+    /// Text traits for line numbers
     public let lineNumbersStyle: LineNumbersStyle? = LineNumbersStyle(font: Font(name: "Menlo", size: 16)!, textColor: lineNumbersColor)
     
+    /// Specifies gutter background color and minimum width
     public let gutterStyle: GutterStyle = GutterStyle(backgroundColor: Color(red: 21/255.0, green: 22/255, blue: 31/255, alpha: 1.0), minimumWidth: 32)
     
+    /// View's font
     public let font = Font(name: "Menlo", size: 15)!
     
+    /// View's background color
     public let backgroundColor = Color(red: 31/255.0, green: 32/255, blue: 41/255, alpha: 1.0)
     
+    /// Function to highlight specific keywords, identifiers, and types
+    /// - Parameter syntaxColorType: specifies color based on parameter or keyword
+    /// - Returns: a color
     public func color(for syntaxColorType: SourceCodeTokenType) -> Color {
         
         switch syntaxColorType {
